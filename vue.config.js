a2c1f0756c1f17a2cd059b9be062c6ee6a11f674
brombaut <ben.rombaut@unb.ca>
2020-03-07 20:28:12 -0400

    Getting domain working

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,15 @@
+const CopyPlugin = require('copy-webpack-plugin');
+
 module.exports = {
   publicPath: '/',
+  configureWebpack: {
+    plugins: [
+      new CopyPlugin([
+        {
+          from: './CNAME',
+          to: './',
+        },
+      ]),
+    ],
+  },
 };
