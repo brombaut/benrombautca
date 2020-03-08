05fba6de1c4e3fa5128233f1b94a6324cbd3f5cc
brombaut <ben.rombaut@unb.ca>
2020-03-07 22:01:34 -0400

    Starting navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,68 @@
+<template>
+    <nav id='navbar'>
+        <div class='icons-section top-icons'>
+            <div class='icon-container'>
+                <font-awesome-icon :icon="['fas', 'home']" />
+            </div>
+        </div>
+        <div class='icons-section middle-icons'>
+            <div class='icon-container'>
+                <font-awesome-icon :icon="['fas', 'street-view']" />
+            </div>
+            <div class='icon-container'>
+                <font-awesome-icon :icon="['far', 'folder-open']" />
+            </div>
+            <div class='icon-container'>
+                <font-awesome-icon :icon="['far', 'map']" />
+            </div>
+            <div class='icon-container'>
+                <font-awesome-icon :icon="['far', 'envelope']" />
+            </div>
+        </div>
+        <div class='icons-section bottom-icons'>
+            <div class='icon-container'>
+                <font-awesome-icon :icon="['fab', 'github']" />
+            </div>
+            <div class='icon-container'>
+                <font-awesome-icon :icon="['fab', 'linkedin']" />
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'NavBar',
+};
+</script>
+
+<style lang='scss'>
+nav {
+    width: 100px;
+    height: auto;
+    background-color: darken(#1c1d25, 5%);
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    padding: 24px 0;
+
+    .icons-section  {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        .icon-container {
+            margin: 16px 0;
+            font-size: 32px;
+            transition: 0.3s;
+
+            &:hover {
+                color: lighten(#2c3e50, 20%);
+                cursor: pointer;
+            }
+        }
+    }
+}
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faUserSecret, faHome, faStreetView } from '@fortawesome/free-solid-svg-icons';
+import { faFolderOpen, faEnvelope, faMap } from '@fortawesome/free-regular-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
+
 import App from './App.vue';
+
+library.add(faUserSecret);
+library.add(faHome);
+library.add(faFolderOpen);
+library.add(faEnvelope);
+library.add(faStreetView);
+library.add(faMap);
+library.add(faGithub);
+library.add(faLinkedin);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
 new Vue({
-  render: (h) => h(App),
+    render: (h) => h(App),
 }).$mount('#app');
