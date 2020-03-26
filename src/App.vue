c0e97025d6bbd5606376c7e1626462253f20cc33
Ben <ben.rombaut@eigen.io>
2020-03-26 11:37:14 -0300

    Removeing bubbles

--- src/App.vue
+++ src/App.vue
@@ -20,8 +20,8 @@ export default {
 
 <style lang="scss">
 #app {
-//   font-family: Avenir, Helvetica, Arial, sans-serif;
     font-family: League Spartan,Helvetica,Arial,sans-serif;
+    font-family: 'Share Tech Mono', Helvetica,Arial,sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
--- src/components/NavBar/NavBarIcon.vue
+++ src/components/NavBar/NavBarIcon.vue
@@ -52,6 +52,7 @@ export default {
     display: flex;
     justify-content: center;
     align-items: center;
+    user-select: none;
 
     &.selected {
         color: darken(#3381db, 15%);
