3192d2e5c96ff9b03a61ccb30063b72167fe50e7
Ben <ben.rombaut@eigen.io>
2020-05-03 09:49:29 -0300

    Adding app background

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,7 @@ export default {
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
+    background: $secondaryDark;
     display: flex;
     height: 100%;
     width: 100%;
