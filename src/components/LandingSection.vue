d8007c66d3a60b473bf9d64cd6c3c2a36635c3fc
Ben <ben.rombaut@eigen.io>
2020-05-03 16:26:52 -0300

    External profile icons

--- src/components/LandingSection.vue
+++ src/components/LandingSection.vue
@@ -4,6 +4,24 @@
       <h1>BEN ROMBAUT</h1>
       <h4>Software Developer</h4>
     </header>
+    <div id="external-profiles-container">
+      <a id="linkedin-link" :href="externalProfiles['linkedin']">
+        <font-awesome-icon
+          id="linkedin-icon"
+          class="icon"
+          @click.stop.prevent="handleExternalProfileClicked('linkedin')"
+          :icon="['fab', 'linkedin']"
+        />
+      </a>
+      <a id="github-link" :href="externalProfiles['github']">
+        <font-awesome-icon
+          id="github-icon"
+          class="icon"
+          @click.stop.prevent="handleExternalProfileClicked('github')"
+          :icon="['fab', 'github']"
+        />
+      </a>
+    </div>
   </section>
 </template>
 
@@ -11,7 +29,22 @@
 import { Component, Vue } from "vue-property-decorator";
 
 @Component
-export default class LandingSection extends Vue {}
+export default class LandingSection extends Vue {
+  private externalProfiles: { [key: string]: string } = {
+    linkedin: "https://www.linkedin.com/in/benjamin-rombaut/",
+    github: "https://github.com/brombaut"
+  };
+
+  handleExternalProfileClicked(key: string) {
+    const url = this.externalProfiles[key];
+    if (url) {
+      const result: Window | null = window.open(url, "_blank");
+      if (result) {
+        result.focus();
+      }
+    }
+  }
+}
 </script>
 
 <style lang="scss">
@@ -19,6 +52,7 @@ export default class LandingSection extends Vue {}
   padding: 0 0 !important;
   height: 100vh;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
 
@@ -31,11 +65,9 @@ export default class LandingSection extends Vue {}
     opacity: 0;
     position: relative;
     box-shadow: 0 2.8px 2.2px rgba(0, 0, 0, 0.034),
-      0 6.7px 5.3px rgba(0, 0, 0, 0.048),
-      0 8.5px 10px rgba(0, 0, 0, 0.06),
-      0 12.3px 7.9px rgba(0, 0, 0, 0.072),
-      0 21.8px 33.4px rgba(0, 0, 0, 0.086);
-      // 0 30px 40px rgba(0, 0, 0, 0.12);
+      0 6.7px 5.3px rgba(0, 0, 0, 0.048), 0 8.5px 10px rgba(0, 0, 0, 0.06),
+      0 12.3px 7.9px rgba(0, 0, 0, 0.072), 0 21.8px 33.4px rgba(0, 0, 0, 0.086);
+    // 0 30px 40px rgba(0, 0, 0, 0.12);
 
     h1 {
       font-size: 3.5rem;
@@ -43,6 +75,36 @@ export default class LandingSection extends Vue {}
 
     h4 {
       font-size: 2rem;
+    }
+  }
+
+  #external-profiles-container {
+    margin-top: 32px;
+
+    a {
+      margin: 0 20px;
+      font-size: 2rem;
+      color: $primary;
+      transition: 0.3s color;
+      animation: slideUp 0.7s ease-out forwards;
+      animation-delay: 0.5s;
+
+      &:hover {
+        color: white;
+        cursor: pointer;
+      }
+    }
+
+    .icon {
+      animation: slideUp 0.7s ease-out forwards;
+      opacity: 0;
+    }
+
+    #linkedin-icon {
+      animation-delay: 1s;
+    }
+    #github-icon {
+      animation-delay: 1.5s;
     }
   }
 
