894e2cfa533f7cad4bdfc4fbddb6280a4a6d7b76
Ben <ben.rombaut@eigen.io>
2020-05-04 15:30:41 -0300

    Refactoring mouse trailer

--- src/components/MouseTrailer.vue
+++ src/components/MouseTrailer.vue
@@ -4,7 +4,7 @@
 
 <script lang="ts">
 /**
- * Ported to this Vue Class componentfrom https://noahyamamoto.com/blog/mousetrailanimation
+ * Ported to this Vue Class Component from https://noahyamamoto.com/blog/mousetrailanimation
  */
 import { Component, Vue } from "vue-property-decorator";
 import Point from "../types/point";
@@ -17,9 +17,16 @@ export default class MouseTrailer extends Vue {
 
   private canvas!: HTMLCanvasElement;
 
+  private ctx!: CanvasRenderingContext2D;
+
+  private points: Point[] = [];
+
+  private duration!: number;
+
   mounted() {
     this.canvas = this.$el as HTMLCanvasElement;
     this.setStateDimensions();
+    this.setDuration();
     window.addEventListener("resize", this.setStateDimensions, false);
     if (matchMedia("(pointer:fine)").matches) {
       this.startAnimation();
@@ -31,80 +38,75 @@ export default class MouseTrailer extends Vue {
     this.cWidth = document.body.clientWidth - 8;
   }
 
-  startAnimation = () => {
-    const canvas = this.$refs.mouseTrailerCanvas as HTMLCanvasElement;
-    const ctx = canvas.getContext("2d");
+  setDuration() {
+    this.duration = 0.7 * (1 * 1000); // Last 80% of a frame per point
+    this.duration /= 60;
+  }
 
-    const points: Point[] = [];
+  startAnimation() {
+    this.canvas = this.$refs.mouseTrailerCanvas as HTMLCanvasElement;
+    this.ctx = this.canvas.getContext("2d") as CanvasRenderingContext2D;
+    document.addEventListener("mousemove", this.mouseMoveListener);
+    this.animatePoints();
+  }
 
-    const addPoint = (x: number, y: number) => {
-      const point = new Point(x, y);
-      points.push(point);
-    };
+  mouseMoveListener(e: MouseEvent) {
+    const x = e.clientX - this.canvas.offsetLeft;
+    const y = e.pageY - this.canvas.offsetTop;
+    this.addPoint(x, y);
+  }
 
-    document.addEventListener(
-      "mousemove",
-      e => {
-        const x = e.clientX - canvas.offsetLeft;
-        const y = e.pageY - canvas.offsetTop;
-        addPoint(x, y);
-      },
-      false
-    );
+  addPoint(x: number, y: number) {
+    const point = new Point(x, y);
+    this.points.push(point);
+  }
 
-    const animatePoints = () => {
-      if (!ctx) {
-        return;
+  animatePoints() {
+    if (!this.ctx) {
+      return;
+    }
+    this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+
+    for (let i = 0; i < this.points.length; ++i) {
+      const point = this.points[i];
+      const lastPoint = this.getLastPoint(i);
+      point.lifetime += 1;
+      if (point.lifetime > this.duration) {
+        this.points.shift();
+      } else {
+        this.animatePoint(point, lastPoint);
       }
-      ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-      let duration = 0.7 * (1 * 1000); // Last 80% of a frame per point
-      duration /= 60;
+    }
+    requestAnimationFrame(this.animatePoints);
+  }
 
-      for (let i = 0; i < points.length; ++i) {
-        const point = points[i];
-        let lastPoint;
+  getLastPoint(i: number) {
+    if (this.points[i - 1] !== undefined) {
+      return this.points[i - 1];
+    }
+    return this.points[i];
+  }
 
-        if (points[i - 1] !== undefined) {
-          lastPoint = points[i - 1];
-        } else {
-          lastPoint = point;
-        }
+  animatePoint(point: Point, lastPoint: Point) {
+    // As the lifetime goes on, lifePercent goes from 0 to 1.
+    const lifePercent = point.lifetime / this.duration;
+    const spreadRate = 7 * (1 - lifePercent);
 
-        point.lifetime += 1;
+    this.ctx.lineJoin = "round";
+    this.ctx.lineWidth = spreadRate;
 
-        if (point.lifetime > duration) {
-          // If the point dies, remove it.
-          points.shift();
-        } else {
-          // Otherwise animate it:
+    // As time increases decrease r and b, increase g to go from purple to green.
+    const red = Math.floor(51 - 51 * lifePercent);
+    const green = Math.floor(129 - 129 * lifePercent);
+    const blue = Math.floor(219 + 219 * lifePercent);
+    this.ctx.strokeStyle = `rgb(${red},${green},${blue}`;
 
-          // As the lifetime goes on, lifePercent goes from 0 to 1.
-          const lifePercent = point.lifetime / duration;
-          const spreadRate = 7 * (1 - lifePercent);
-
-          ctx.lineJoin = "round";
-          ctx.lineWidth = spreadRate;
-
-          // As time increases decrease r and b, increase g to go from purple to green.
-          const red = Math.floor(51 - 51 * lifePercent);
-          const green = Math.floor(129 - 129 * lifePercent);
-          const blue = Math.floor(219 + 219 * lifePercent);
-          ctx.strokeStyle = `rgb(${red},${green},${blue}`;
-
-          ctx.beginPath();
-
-          ctx.moveTo(lastPoint.x, lastPoint.y);
-          ctx.lineTo(point.x, point.y);
-
-          ctx.stroke();
-          ctx.closePath();
-        }
-      }
-      requestAnimationFrame(animatePoints);
-    };
-
-    animatePoints();
-  };
+    this.ctx.beginPath();
+    this.ctx.moveTo(lastPoint.x, lastPoint.y);
+    this.ctx.lineTo(point.x, point.y);
+    this.ctx.stroke();
+    this.ctx.closePath();
+  }
 }
 </script>
 
