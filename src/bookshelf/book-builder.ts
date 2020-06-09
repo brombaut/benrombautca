ae8e397fd57c11defc637f1899cb77c16d10699a
Ben Rombaut <rombaut.benj@gmail.com>
2020-06-09 11:41:39 -0300

    Pushing what I have

--- src/bookshelf/book-builder.ts
+++ src/bookshelf/book-builder.ts
@@ -9,12 +9,7 @@ class BookBuilder {
   }
 
   build(): Book {
-    console.log(this._bookDto.title, " - ", this._bookDto.isbn13);
     return new Book(this._bookDto);
-  }
-
-  private buildOpenLibraryImageUrl(size: string): string {
-    return `http://covers.openlibrary.org/b/isbn/${this._bookDto.isbn}-${size}.jpg`;
   }
 }
 
