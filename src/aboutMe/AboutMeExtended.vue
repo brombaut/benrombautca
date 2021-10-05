99758d678c6ca0c94f6a1fb68f68c2bfceda6ce7
Ben Rombaut <rombaut.benj@gmail.com>
2021-10-04 21:31:42 -0400

    Cleaning up AboutMeExtended - Closes #239

--- src/aboutMe/AboutMeExtended.vue
+++ src/aboutMe/AboutMeExtended.vue
@@ -7,7 +7,6 @@
 
 <script lang="ts">
 import Vue from "vue";
-import { bus } from "@/main";
 import AboutMeSection from "@/aboutMe/AboutMeSection.vue";
 import WorkEducationSection from "@/workEducation/WorkEducationSection.vue";
 
@@ -16,40 +15,6 @@ export default Vue.extend({
   components: {
     AboutMeSection,
     WorkEducationSection,
-  },
-  computed: {
-    curRoute(): string {
-      return this.$route.name || "";
-    },
-  },
-  methods: {
-    getElToScrollTo(): HTMLElement | null {
-      let result: HTMLElement | null;
-      switch (this.curRoute) {
-      case "work": {
-        result = document.querySelector("#work");
-        break;
-      }
-      case "education": {
-        result = document.querySelector("#education");
-        break;
-      }
-      default: {
-        result = document.querySelector("#site-header");
-      }
-      }
-      return result;
-    },
-    scrollToRouteElement(): void {
-      const elToScrollTo: HTMLElement | null = this.getElToScrollTo();
-      if (elToScrollTo) {
-        elToScrollTo.scrollIntoView({ behavior: "smooth" });
-      }
-    },
-  },
-  mounted() {
-    setTimeout(this.scrollToRouteElement, 150);
-    bus.$on("routeClicked", this.scrollToRouteElement);
   },
 });
 </script>
