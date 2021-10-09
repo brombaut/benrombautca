76b7f5326d2fa27782c13066840f7cd5d9a7d051
Ben Rombaut <rombaut.benj@gmail.com>
2021-10-09 18:02:34 -0400

    Start work on #240. Not sure how I like the look of it, so just not showing on frontend for now

--- src/articles/ArticlesSection.vue
+++ src/articles/ArticlesSection.vue
@@ -5,6 +5,13 @@
       icon="pen-square"
       subtext="A collection of how-to guides and notes I've written on different topics, mostly so that I can use them as references later."/>
     <div class="section-body">
+      <div id="tags-filter" v-if="false">
+        <h3 class='header'>Tags Filter</h3>
+        <hr>
+        <div class="tags-filter__container">
+          <Tag v-for="tag in listOfUniqueTags" :key="tag" :tag="tag" />
+        </div>
+      </div>
       <div class="articles-list">
         <ArticleCard
           v-for="article in articlesToDisplay"
@@ -20,6 +27,7 @@
 import Vue from "vue";
 import SectionHeader from "../shared/SectionHeader.vue";
 import ArticleCard from "./ArticleCard.vue";
+import Tag from "@/shared/Tag.vue";
 import { AuthoredArticlesProxy, AuthoredArticle } from "./AuthoredArticlesProxy";
 
 export default Vue.extend({
@@ -27,6 +35,7 @@ export default Vue.extend({
   components: {
     SectionHeader,
     ArticleCard,
+    Tag,
   },
   data() {
     return {
@@ -41,6 +50,27 @@ export default Vue.extend({
         .sort((a: AuthoredArticle, b: AuthoredArticle) => {
           return b.createdAt.getTime() - a.createdAt.getTime();
         });
+    },
+    listOfUniqueTags(): string[] {
+      const arrayOfTagsArrays: string[][] = this.authoredArticles.map((aa: AuthoredArticle) => aa.tags);
+      type TagCount = {
+        tag: string,
+        count: number
+      };
+      const result: TagCount[] = [];
+      arrayOfTagsArrays.forEach((tagsArr: string[]) => {
+        tagsArr.forEach((tag: string) => {
+          if (!result.find((tc: TagCount) => tc.tag === tag)) {
+            result.push({ tag, count: 0 });
+          }
+          const matchingTC: TagCount | undefined = result.find((tc: TagCount) => tc.tag === tag);
+          if (matchingTC) {
+            matchingTC.count++;
+          }
+        });
+      });
+
+      return result.map((tc: TagCount) => `${tc.tag} (${tc.count})`);
     },
   },
   methods: {
@@ -59,8 +89,24 @@ export default Vue.extend({
 
   .section-body {
     display: flex;
-    flex-direction: row;
-    align-items: center;
+    flex-direction: column;
+    align-items: flex-start;
+
+    #tags-filter {
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-start;
+      background-color: darken($secondary, 2%);
+      padding: 8px 16px;
+      border-radius: 16px;
+      margin: 0px 20px;
+
+      .tags-filter__container{
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+      }
+    }
 
     .articles-list {
       display: flex;
