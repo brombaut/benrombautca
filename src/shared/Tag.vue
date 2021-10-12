700d0fb36fd3f9426520318c2051300667f51cc9
Ben Rombaut <rombaut.benj@gmail.com>
2021-10-11 22:51:06 -0400

    Adding coloured tags

--- src/shared/Tag.vue
+++ src/shared/Tag.vue
@@ -1,28 +1,68 @@
 <template>
-  <div class="tag">{{ tag }}</div>
+  <div class="tag" :style="tagStyle">{{ tag }}</div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+
+class TagColor {
+  backgroundColor: string;
+  color: string;
+  constructor(backgroundColor: string, color: string) {
+    this.backgroundColor = backgroundColor;
+    this.color = color;
+  }
+}
 
 export default Vue.extend({
   name: "Tag",
   props: {
     tag: String,
   },
+  computed: {
+    tagStyle(): Object {
+      return {
+        backgroundColor: this.tagColors.backgroundColor,
+        color: this.tagColors.color,
+      };
+    },
+    tagColors(): TagColor {
+      switch (this.$props.tag) {
+      case ("how-to"): return new TagColor("rgb(254, 255, 165)", "rgb(179, 0, 71)");
+      case ("init"): return new TagColor("rgb(245, 57, 0)", "rgb(255, 255, 255)");
+      case ("notes"): return new TagColor("#d6d9e0", "#606570");
+      case ("typescript"): return new TagColor("rgb(35, 74, 132)", "rgb(255, 255, 255)");
+      case ("npm"): return new TagColor("rgb(137, 6, 6)", "rgb(255, 255, 255)");
+      case ("publish"): return new TagColor("rgb(86, 39, 101)", "rgb(255, 255, 255)");
+      case ("deploy"): return new TagColor("rgb(6, 181, 0)", "rgb(255, 255, 255)");
+      case ("github-actions"): return new TagColor("rgb(247, 223, 30)", "rgb(0, 0, 0)");
+      case ("github-pages"): return new TagColor("rgb(86, 39, 101)", "rgb(255, 255, 255)");
+      case ("statistics"): return new TagColor("rgb(237, 21, 86)", "rgb(255, 255, 255)");
+      case ("example-repo"): return new TagColor("rgb(17, 58, 98)", "rgb(255, 255, 255)");
+      case ("software-architecture"): return new TagColor("rgb(0, 0, 0)", "rgb(255, 255, 255)");
+      case ("design-patterns"): return new TagColor("rgb(41, 101, 241)", "rgb(255, 255, 255)");
+      case ("codecov"): return new TagColor("rgb(87, 215, 0)", "rgb(255, 255, 255)");
+      case ("testing"): return new TagColor("rgb(0, 131, 53)", "rgb(255, 255, 255)");
+      case ("cron"): return new TagColor("rgb(0, 0, 0)", "rgb(255, 255, 255)");
+      case ("scheduled"): return new TagColor("rgb(224, 235, 245)", "rgb(4, 60, 111)");
+      case ("correlation"): return new TagColor("rgb(42, 37, 102)", "rgb(255, 255, 255)");
+      case ("residuals"): return new TagColor("rgb(108, 0, 185)", "rgb(255, 255, 255)");
+      case ("linear-regression"): return new TagColor("rgb(34, 34, 34)", "rgb(97, 218, 246)");
+      default: return new TagColor("#1d5ca4", "#f1f5fa");
+      }
+    },
+  },
 });
 </script>
 
 <style lang="scss">
 .tag {
-  font-size: 0.8em;
+  font-size: 0.9em;
   font-weight: bold;
   margin: 4px 0;
   margin-right: 12px;
-  padding: 8px 12px;
-  border-radius: 8px;
-  color: $secondary;
-  background-color: $primaryDark;
+  padding: 6px 8px;
+  border-radius: 6px;
 }
 
 @media only screen and (max-width: $SMALL_DISPLAY_SIZE) {
