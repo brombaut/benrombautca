b63ced734885a1cb986f9bf05c5f6ef0b680807d
Ben Rombaut <rombaut.benj@gmail.com>
2022-01-28 17:09:17 -0500

    Updating about me

--- src/aboutMe/aboutMe.ts
+++ src/aboutMe/aboutMe.ts
@@ -1,16 +1,16 @@
 import { AboutMe } from "./about-me";
-
+/* eslint-disable */
 const aboutMe: AboutMe = {
   description: `
   <p>
-  I am currently a Master of Science Candidate at the <a href="https://sail.cs.queensu.ca/" target="_blank">Software Analysis & Intelligence Lab (SAIL)</a> at <a href="https://www.queensu.ca/" target="_blank">Queen's University</a> in Kingston, Ontario, under the supervision of <a href="https://research.cs.queensu.ca/home/ahmed/home/" target="_blank">Dr. Ahmed E. Hassan</a>.
+  I am a Master of Science Candidate at the <a href="https://sail.cs.queensu.ca/" target="_blank">Software Analysis & Intelligence Lab (SAIL)</a> at <a href="https://www.queensu.ca/" target="_blank">Queen's University</a> in Kingston, Ontario, under the supervision of <a href="https://research.cs.queensu.ca/home/ahmed/home/" target="_blank">Dr. Ahmed E. Hassan</a>. I'm also a Software Engineering Researcher at the Centre for Software Excellence with <a href="https://www.huawei.com/ca/" target="_blank">Huawei Canada</a>.
   </p>
   <p>
   I earned a <a href="https://www.unb.ca/fredericton/engineering/undergrad/swe/" target="_blank">Bachelor of Science in Software Engineering</a> from the <a href="https://www.unb.ca/" target="_blank">University of New Brunswick</a> in 2019.
   I also graduated from the <a href="https://www.unb.ca/fredericton/cs/" target="_blank">Faculty of Computer Science</a> <a href="https://www.unb.ca/fredericton/cs/coop/index.html" target="_blank">Co-operative Education Program</a> alongside my degree, completing multiple work terms in both academia and industry.
   </p>
   <p>
-  I enjoy learning about a broad range of topics in computer science and software engineering. I am primarily interested in mining software repositories (MSR), software ecosystems & dependency management, data analytics & machine learning,  software reliability & security, and software maintenance & evolution.
+  I enjoy learning about a broad range of topics related to computer science and software engineering. I am primarily interested in software supply chain management, software bots, data analytics & machine learning, and software reliability & security. I also have budding interests in software engineering for healthcare and medical informatics.
   </p>
   `,
   imageFileName: "benrombaut.jpg",
--- src/footer/last-deployed.ts
+++ src/footer/last-deployed.ts
@@ -1,1 +1,1 @@
-export default "19/01/2022";
+export default "28/01/2022";
