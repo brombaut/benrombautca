b5efaff0dac8408560701cd696ea7d6d6f553c90
Ben Rombaut <rombaut.benj@gmail.com>
2025-01-04 19:20:25 -0500

    Try to force north america rendering

--- src/bookshelf/syncer_v2/00_goodreads_scraper.py
+++ src/bookshelf/syncer_v2/00_goodreads_scraper.py
@@ -42,7 +42,8 @@ def dict_to_json_file(data, file_path):
 def get_html(url):
     logger.debug(f"[get_html] Fetching HTML for URL: {url}")
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+        'Accept-Language': 'en-US,en;q=0.9'
     }
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
