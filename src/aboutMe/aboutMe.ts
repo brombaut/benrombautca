ef1ae289b93fc31b43edc6ec19268a83e7f8e6f6
Ben Rombaut <rombaut.benj@gmail.com>
2025-03-17 20:44:01 -0400

    Update about me

--- src/aboutMe/aboutMe.ts
+++ src/aboutMe/aboutMe.ts
@@ -3,10 +3,14 @@ import { AboutMe } from "./about-me";
 const aboutMe: AboutMe = {
   description: `
   <p>
-  I'm a Software Engineering Researcher with the Centre for Software Excellence at Huawei Canada, where I work with a team researching bleeding edge topics and tools related to software engineering for high-performance computing and heterogeneous computing systems. I earned my M.Sc. from the School of Computing at Queen's University in 2022, and my B.Sc. in Software Engineering from the University of New Brunswick in 2019.
+  I'm a Software Engineering Researcher with the Centre for Software Excellence at Huawei Canada, living in Montreal, Quebec, 
+  where I work with a team researching bleeding edge topics and tools related to AI for software engineering (AI4SE) and software engineering for AI systems (SE4AI). 
+  I earned my M.Sc. from the School of Computing at Queen's University in 2022, and my B.Sc. in Software Engineering from the University of New Brunswick in 2019.
   </p>
   <p>
-  I enjoy learning about a broad range of topics related to computer science and software engineering. Aside from my work, I am primarily interested in software supply chain management, software bots, data analytics & machine learning, and software reliability & security. I also have budding interests in software engineering in healthcare and medical informatics.
+  I enjoy learning about a broad range of topics related to computer science and software engineering. 
+  Aside from my work, I am primarily interested in software supply chain management, software bots, and software reliability & security. 
+  I also have budding interests in software engineering in healthcare and medical informatics.
   </p> 
   `,
   imageFileName: "benrombaut.jpg",
